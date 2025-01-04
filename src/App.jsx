f2775bf08b3aa35e980cb44b8cb273076036497d
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-01-04 11:06:29 +0100

    add login form

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import blogService from "./services/blogs";
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
 
   return (
     <div>
+      <h2>log in to the application</h2>
+      <form>
+        Username:
+        <input type="test" name="" id="" />
+        <br />
+        Password:
+        <input type="password" name="" id="" />
+        <br />
+        <input type="submit" value="login" />
+      </form>
       <h2>blogs</h2>
-      {blogs.map(blog =>
+      {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
-      )}
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
