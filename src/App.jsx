8dc56683a24f7d23bc5cf8eff5259682e4f89749
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-01-04 21:51:42 +0100

    add wrapper class for login form container element
    styled login form

--- src/App.jsx
+++ src/App.jsx
@@ -107,28 +107,37 @@ const App = () => {
 
   if (user === null) {
     return (
-      <div>
+      <>
         <Notification message={errorMessage} type="error" />
         <Notification message={successMessage} type="success" />
-        <h2>log in to the application</h2>
-        <form onSubmit={handleLogin}>
-          Username:
-          <input
-            onChange={({ target }) => setUsername(target.value)}
-            type="text"
-            value={username}
-          />
-          <br />
-          Password:
-          <input
-            onChange={({ target }) => setPassword(target.value)}
-            type="password"
-            value={password}
-          />
-          <br />
-          <input type="submit" value="login" />
-        </form>
-      </div>
+        <div className="login-form-container">
+          <form onSubmit={handleLogin} className="login-form">
+            <h2>log in to the application</h2>
+
+            <div className="form-group">
+              <label htmlFor="username">Username:</label>
+              <input
+                id="username"
+                onChange={({ target }) => setUsername(target.value)}
+                type="text"
+                value={username}
+                className="form-input"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password:</label>
+              <input
+                id="password"
+                onChange={({ target }) => setPassword(target.value)}
+                type="password"
+                value={password}
+                className="form-input"
+              />
+            </div>
+            <input type="submit" value="Login" className="form-submit" />
+          </form>
+        </div>
+      </>
     );
   }
 
