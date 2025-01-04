c2d4f9c6bff2679ea892112e6526d03646223b09
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-01-04 15:25:39 +0100

    feat: Add service function to create new blogs
      - Implemented `addNewBlog` function to send POST requests to the `/api/blogs` endpoint.
      - Added error handling to manage specific server response errors and provide default error messages.
      - Ensured the service throws error objects containing message and status code for consistent error handling.

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,9 +1,30 @@
-import axios from 'axios'
-const baseUrl = '/api/blogs'
+import axios from "axios";
+const baseUrl = "/api/blogs";
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
 
-export default { getAll }
+const addNewBlog = async (payload) => {
+  try {
+    const response = await axios.poat(baseUrl, payload);
+    return {
+      success: true,
+      data: response.data,
+      status: response.status,
+    };
+  } catch (error) {
+    const errorMessage =
+      error.response?.data?.message || "An unknown error occured";
+    const stausCode = error.response?.status || 500;
+
+    throw {
+      success: false,
+      message: errorMessage,
+      status: stausCode,
+    };
+  }
+};
+
+export default { getAll, addNewBlog };
