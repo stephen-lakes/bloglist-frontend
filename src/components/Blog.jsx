fc21cd0423c7a3bbc75c0cd9c0bced877116f420
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-01-04 21:41:17 +0100

    chore: restructure Blog component
    added styles

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,7 @@
 const Blog = ({ blog }) => (
   <div className="blog-container">
     <h2 className="blog-title">{blog.title}</h2>
-    <p className="blog-author">{blog.author}</p>
+    <p className="blog-author">author {blog.author}</p>
   </div>  
 );
 
