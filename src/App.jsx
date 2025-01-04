e4a730440d8ed49099d3caf51ca458acdd3b2cbd
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-01-04 14:58:24 +0100

    add blog creation form

--- src/App.jsx
+++ src/App.jsx
@@ -86,6 +86,18 @@ const App = () => {
       <p>
         {user.name} logged in <button onClick={handleLogout}>logout</button>
       </p>
+
+      <h2>create new</h2>
+      <form>
+        title: <input type="text" />
+        <br />
+        author: <input type="text" />
+        <br />
+        url: <input type="text" />
+        <br />
+        <input type="submit" value="create" />
+      </form>
+
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
