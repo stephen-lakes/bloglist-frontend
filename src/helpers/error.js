fb9f64a49622893d194553c7386b8a082dd15c24
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-01-04 17:53:21 +0100

    add 400 error case

--- src/helpers/error.js
+++ src/helpers/error.js
@@ -1,7 +1,10 @@
 const handleError = (error) => {
   switch (error.status) {
+    case 400:
+      console.error("Bad Request");
+      break;
     case 404:
-      console.error(`User Not Found`);
+      console.error("User Not Found");
       break;
     case 401:
       console.error(`Invalid Login Credentials`);
