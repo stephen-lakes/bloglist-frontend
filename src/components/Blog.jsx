8193a299ff78be38ad43d2b908ce0460e4ab4b25
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-01-04 22:11:35 +0100

    implement component to handle blog creation

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -2,7 +2,44 @@ const Blog = ({ blog }) => (
   <div className="blog-container">
     <h2 className="blog-title">{blog.title}</h2>
     <p className="blog-author">author {blog.author}</p>
-  </div>  
+  </div>
 );
+
+export const AddNewBlogForm = ({ addBlog, title, author, url }) => {
+  return (
+    <>
+      <form onSubmit={addBlog}>
+        title:
+        <input
+          type="text"
+          value={title}
+          onChange={({ target }) => {
+            setTitle(target.value);
+          }}
+        />
+        <br />
+        author:
+        <input
+          type="text"
+          value={author}
+          onChange={({ target }) => {
+            setAuthor(target.value);
+          }}
+        />
+        <br />
+        url:
+        <input
+          type="text"
+          value={url}
+          onChange={({ target }) => {
+            setUrl(target.value);
+          }}
+        />
+        <br />
+        <input type="submit" value="create" />
+      </form>
+    </>
+  );
+};
 
 export default Blog;
