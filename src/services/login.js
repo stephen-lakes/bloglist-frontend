7e891433b7ead0db44d6bbbbf18dec32bd5d2df6
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-01-04 13:32:05 +0100

    feat: Improve login function to handle specific error messages
    - Updated `login` function to display proper error messages such as
      "Incorrect password" and "Username not found".
    - Added error handling to check for specific error responses from the
        server.
    - Included default error messages for network or server issues.

--- src/services/login.js
+++ src/services/login.js
@@ -1,10 +1,23 @@
 import axios from "axios";
 const baseUrl = "/api/login";
 
-
 const login = async (credentials) => {
-  const response = await axios.post(baseUrl, credentials);
-  return response.data;
+  try {
+    const response = await axios.post(baseUrl, credentials);
+    return response.data;
+  } catch (error) {
+    if (error.response && error.response.data)
+      return {
+        success: false,
+        message: error.response.data.message || "An unknown error occured",
+      };
+    else {
+      return {
+        success: false,
+        message: "Network error or server not responding",
+      };
+    }
+  }
 };
 
 export default { login };
