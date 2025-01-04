0793fa6f4520728865b163e1b39b04c5b8569785
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-01-04 22:16:33 +0100

    add styles to add blog form

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -7,38 +7,41 @@ const Blog = ({ blog }) => (
 
 export const AddNewBlogForm = ({ addBlog, title, author, url }) => {
   return (
-    <>
-      <form onSubmit={addBlog}>
-        title:
-        <input
-          type="text"
-          value={title}
-          onChange={({ target }) => {
-            setTitle(target.value);
-          }}
-        />
-        <br />
-        author:
-        <input
-          type="text"
-          value={author}
-          onChange={({ target }) => {
-            setAuthor(target.value);
-          }}
-        />
-        <br />
-        url:
-        <input
-          type="text"
-          value={url}
-          onChange={({ target }) => {
-            setUrl(target.value);
-          }}
-        />
-        <br />
-        <input type="submit" value="create" />
+    <div className="add-blog-form-container">
+      <form onSubmit={addBlog} className="add-blog-form">
+        <div className="form-group">
+          <label htmlFor="title">Title:</label>
+          <input
+            id="title"
+            type="text"
+            value={title}
+            onChange={({ target }) => setTitle(target.value)}
+            className="form-input"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="author">Author:</label>
+          <input
+            id="author"
+            type="text"
+            value={author}
+            onChange={({ target }) => setAuthor(target.value)}
+            className="form-input"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="url">URL:</label>
+          <input
+            id="url"
+            type="text"
+            value={url}
+            onChange={({ target }) => setUrl(target.value)}
+            className="form-input"
+          />
+        </div>
+        <input type="submit" value="Create" className="form-submit" />
       </form>
-    </>
+    </div>
   );
 };
 
