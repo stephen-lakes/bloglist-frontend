5fe6f742afeea839ff4668cf0c0d7b428ecae1c9
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-01-06 10:10:01 +0100

    add service to update blog

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -37,4 +37,33 @@ const addNewBlog = async (payload) => {
   }
 };
 
+const updateBlog = async (blogId, payload) => {
+  try {
+    const token = getToken();
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    console.log("BEFORE =>>>");
+    const response = await axios.put(baseUrl + `/${blogId}`, payload, config);
+    console.log("AFTER =>>>");
+    return {
+      success: true,
+      data: response.data,
+      status: response.status,
+    };
+  } catch (error) {
+    const errorMessage =
+      error.response?.data?.message || "An unknown error occured";
+    const stausCode = error.response?.status || 500;
+
+    throw {
+      success: false,
+      message: errorMessage,
+      status: stausCode,
+    };
+  }
+};
+
 export default { getAll, addNewBlog };
