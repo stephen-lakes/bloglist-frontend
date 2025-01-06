8a2c95e344ac880adf5b6ad429c73715fc90764d
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-01-06 03:52:12 +0100

    Create Notification component

---
+++ src/components/Notification.jsx
@@ -0,0 +1,14 @@
+const Notification = ({ message, type }) => {
+  if (message === null) {
+    return null;
+  }
+  return (
+    <>
+      <div className={`${type === "success" ? "success" : "error"}`}>
+        {message}
+      </div>
+    </>
+  );
+};
+
+export default Notification;
