8d1feea7dc485cff3246518ccefecc87f3ddc802
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-01-06 14:04:40 +0100

    update the rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -38,8 +38,8 @@ module.exports = {
   rules: {
     indent: ["error", 2],
     "linebreak-style": ["error", "unix"],
-    quotes: ["error", "single"],
-    semi: ["error", "never"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
     eqeqeq: "error",
     "no-trailing-spaces": "error",
     "object-curly-spacing": ["error", "always"],
